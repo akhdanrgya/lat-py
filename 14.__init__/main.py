439313fed3eb0da2d5ca67a4bcb656fc9cdd3711
akhdanrgya <akhdan.anargya@gmail.com>
2023-11-12 21:26:46 +0700

    add init

---
+++ 14.__init__/main.py
@@ -0,0 +1,9 @@
+import package
+
+tambah = package.math.tambah(1,2)
+
+print(f"hasil tambah adalah : {tambah}")
+
+test = package.test.anjay("akhdan")
+
+print(f"hasil dari test adalah : {test}")
---
+++ 14.__init__/package/test.py
@@ -0,0 +1,6 @@
+"""Modul Test"""
+
+def anjay(message):
+    output = f"Hallo nama saya adalah {message}"
+
+    return output
